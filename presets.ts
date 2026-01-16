974a37f9385dc7331af27706de912a0d760c4549
7AMDI <97978397+74mdi@users.noreply.github.com>
2026-01-16 18:50:35 +0100

    Update webhook and token configurations in presets

--- presets.ts
+++ presets.ts
@@ -10,9 +10,9 @@ export const PRESETS: Preset[] = [
   {
     name: '7amdi',
     config: {
-      discordWebhook: 'https://discord.com/api/webhooks/1234567890/abcdefg',
-      telegramBotToken: '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11',
-      telegramChatId: '-1001234567890'
+      discordWebhook: 'https://discord.com/api/webhooks/1398383141630120076/ACqj56SP8VfW_zpz5vLSBkooe_VF-hIOSLQLV_LKIXPq199C_4gD6jBOkOlBc8J5yWnK',
+      telegramBotToken: '8374992747:AAE_omVXZmO750W6RKq941XkCJo9UIQ73B0',
+      telegramChatId: '7299643916'
     }
   },
   {
